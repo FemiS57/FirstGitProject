cae690eb4139a7107d573d6c9264db0321bb8e41
FemiS57 <feminasdawood@gmail.com>
2023-11-27 17:54:17 +0530

    methods are added

---
+++ src/org/git/Characters.java
@@ -0,0 +1,37 @@
+package org.git;
+
+public class Characters {
+	
+	private void Carden() {
+		System.out.println("Carden the High King of Elfhame ");
+    }
+	private void Rhysand() {
+		System.out.println("Rhysand the High Lord of Nightcourt ");
+
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
